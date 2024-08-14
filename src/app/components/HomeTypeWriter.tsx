9a5b79af62b7a22c2394d1a0306459cd255d4d36
fiardiel <rafi.ardiel@ui.ac.id>
2024-08-14 23:34:12 +0700

    FEAT: styled the start of the front page

--- src/app/components/HomeTypeWriter.tsx
+++ src/app/components/HomeTypeWriter.tsx
@@ -1,29 +1,29 @@
 'use client'
 
-import { Poppins, Space_Mono, JetBrains_Mono } from 'next/font/google';
 import React from 'react'
 import TypeIt from 'typeit-react';
 
-const spaceMono = Space_Mono({ subsets: ["latin"], weight: '700' });
-const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ] });
-const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800' ] });
 
-const HomeTypeWriter = () => {
+interface HomeTypeWriterProps { 
+    className?: string;
+}
+
+const HomeTypeWriter: React.FC<HomeTypeWriterProps> = ({className}) => {
     return (
-        <div className={`${jetbrainsMono.className} mb-4 text-3xl font-semibold md:text-start text-center`}>
+        <div className={`text-3xl md:text-start text-center ${className}`}>
             <TypeIt options={{
                 strings: [
-                    "<h3> Hi, I'm Rafi Ardiel! 👋 </h3>",
-                    "<p> Full-stack Developer 💻 </p> ",
-                    "<p> Computer Science @Fasilkom UI 🎓 </p>",
+                    "I'm Rafi Ardiel Erinaldi 👋",
+                    "I'm a Full-stack Developer 💻",
+                    "I'm a CSUI2022 Student 🎓"
                 ],
                 loop: true,
                 speed: 40,
                 deleteSpeed: 30,
                 html: false,
                 breakLines: false,
-                nextStringDelay: 2000,
-                loopDelay: 3000,
+                nextStringDelay: 4000,
+                loopDelay: [4000, 4000, 4000],
             }}>
             </TypeIt>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,49 @@
 import { Button } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
 import HomeTypeWriter from "./components/HomeTypeWriter";
 import { Image } from "@nextui-org/image"
 import { MdArrowOutward } from "react-icons/md";
-import Link from "next/link";
+import { Poppins, Space_Mono, JetBrains_Mono } from 'next/font/google';
+import React from 'react'
+
+const spaceMono = Space_Mono({ subsets: ["latin"], weight: '700' });
+const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ] });
+const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800' ] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-6">
-      <div className="flex flex-col justify-center md:flex md:items-center w-full min-h-screen lg:w-[1024px] px-0 md:-translate-y-20 lg:px-6">
-        <div className="md:grid md:grid-cols-[auto,1fr] md:auto-cols-min md:gap-32 flex flex-col-reverse items-center">
+    <main className="flex flex-col min-h-screen items-center justify-between px-6">
+      <div className="flex flex-col justify-center md:flex md:items-center w-full lg:w-[1024px] px-0 lg:px-6 py-10 pt-16">
+        <div className="md:grid md:grid-cols-[auto,1fr] md:auto-cols-min md:gap-20 flex flex-col-reverse items-center">
           <div className="flex flex-col items-center md:items-start w-full md:w-full">
-            <HomeTypeWriter />
-            <p className="text-md font-light text-gray-400 md:text-start text-center mb-6">
+
+            <h1 className={`font-sans text-4xl font-medium`}>Hello<span className="text-blue-600">, World!</span></h1>
+            <HomeTypeWriter className="mt-2 font-medium"/>
+            
+            <p className="text-md font-light text-gray-400 md:text-start text-center mb-6 mt-4">
               A Full-stack Developer specializing in Next.js and
               Django Rest Framework / Springboot. Currently a Web
-              Developer Intern at PT. Samakta Mitra
+              Developer Intern at <Link underline="always" href="https://www.linkedin.com/company/samakta-mitra---itaas/" className="decoration-2 underline hover:decoration-red-500 text-gray-400"> PT. Samakta Mitra</Link> 
             </p>
-            <div className="flex justify-center gap-4 flex-wrap">
-              <Button as={Link} href={`/profile`} color="primary" variant="flat" className="hover:bg-primary hover:text-white"> About me <MdArrowOutward/> </Button>
-              <Button as={Link} href={`/projects`} color="primary" variant="flat" className="hover:bg-primary hover:text-white"> My Projects <MdArrowOutward/>  </Button>
-              <Button as={Link} href={`/experiences`} color="primary" variant="flat" className="hover:bg-primary hover:text-white"> Experiences <MdArrowOutward/>  </Button>
+
+            <div className="flex justify-start gap-4 flex-wrap">
+              <Button as={Link} href={`/profile`} variant="bordered" className="shadow-lg shadow-blue-500/50 transition hover:scale-105 duration-150 hover:-translate-y-1 decoration-blue-500">
+                About me <MdArrowOutward />
+              </Button>
+              <Button as={Link} href={`/projects`} variant="bordered" className="shadow-lg shadow-blue-500/50 transition hover:scale-105 duration-150 hover:-translate-y-1 decoration-blue-500">
+                My Projects <MdArrowOutward />
+              </Button>
+              <Button as={Link} href={`/experiences`} variant="bordered" className="shadow-lg shadow-blue-500/50 transition hover:scale-105 duration-150 hover:-translate-y-1 decoration-blue-500">
+                Experiences <MdArrowOutward />
+              </Button>
             </div>
+
           </div>
-          <div className="w-[200px] h-[200px] md:w-[250px] md:h-[250px] mb-10 md:mb-0 opacity-70 transition hover:opacity-80">
+          <div className="w-[200px] h-[200px] md:w-[250px] md:h-[250px] mb-10 md:mb-0 opacity-70 grayscale transition hover:grayscale-0 hover:opacity-100">
             <Image
-              src="/images/rafibulet-modified.png"
+              src="/images/rafibulet.png"
               alt="rafi ardiel"
-              className="rounded-full"
+              className="rounded-full shadow-2xl shadow-white/50"
             ></Image>
           </div>
         </div>
