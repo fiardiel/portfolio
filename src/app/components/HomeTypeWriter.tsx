6e5f929e27298101bb72eaca65778c2e88cc999b
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-08-14 21:48:30 +0700

    FEAT: styled the typewriter

--- src/app/components/HomeTypeWriter.tsx
+++ src/app/components/HomeTypeWriter.tsx
@@ -2,7 +2,7 @@
 
 import { Poppins, Space_Mono, JetBrains_Mono } from 'next/font/google';
 import React from 'react'
-import Typewriter from 'typewriter-effect';
+import TypeIt from 'typeit-react';
 
 const spaceMono = Space_Mono({ subsets: ["latin"], weight: '700' });
 const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ] });
@@ -11,18 +11,21 @@ const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ['100', '200'
 const HomeTypeWriter = () => {
     return (
         <div className={`${jetbrainsMono.className} mb-4 text-3xl font-semibold md:text-start text-center`}>
-            <Typewriter options={{
+            <TypeIt options={{
                 strings: [
-                    " < Hi, I'm Rafi Ardiel! />",
-                    " < Full-stack Developer />",
-                    " < Computer Science @Fasilkom UI />",
+                    "<h3> Hi, I'm Rafi Ardiel! 👋 </h3>",
+                    "<p> Full-stack Developer 💻 </p> ",
+                    "<p> Computer Science @Fasilkom UI 🎓 </p>",
                 ],
-                autoStart: true,
                 loop: true,
-                delay: 40,
-                deleteSpeed: 20,
+                speed: 40,
+                deleteSpeed: 30,
+                html: false,
+                breakLines: false,
+                nextStringDelay: 2000,
+                loopDelay: 3000,
             }}>
-            </Typewriter>
+            </TypeIt>
         </div>
     )
 }
