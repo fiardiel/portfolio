815e2123ff7d14e8fb6bd42b137e33e3615e2f45
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-08-15 22:25:11 +0700

    REFACTOR: fixed the classes ordering in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Top from '@/components/home/top/Top';
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-between px-6">
+    <main className="flex flex-col items-center justify-between min-h-screen px-6">
       <div className="flex flex-col justify-center md:flex md:items-center w-full lg:w-[1024px] px-0 lg:px-6 py-10 pt-16">
         <Top/>
       </div>
