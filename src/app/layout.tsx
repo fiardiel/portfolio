86d2c272f3238dcc3400efe77c4e76eae5e438f3
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-08-24 09:08:00 +0700

    CHORE: transfer the body scroll classes to the globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full dark">
-      <body className={`${inter.className} dark bg-black text-foreground overscroll-none scroll-smooth`}>
+      <body className={`${inter.className} dark bg-black text-foreground`}>
         <Providers font={inter.className}>
           <Navbar />
           <SmallNavbar />
